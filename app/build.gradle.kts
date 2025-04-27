b6032ed557c47695a81c7d78c32059b794deccdf
hadasa <hadasagoralnik@gmail.com>
2025-04-27 22:52:54 +0300

    list item propil users

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
     kapt ("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
     implementation ("com.google.code.gson:gson:2.9.0")
+    implementation ("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
 
     implementation("com.github.bumptech.glide:glide:4.16.0")
     kapt("com.github.bumptech.glide:compiler:4.16.0")
